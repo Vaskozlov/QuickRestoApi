13de2ef39c93cd6e31c9050b127ed5eb42bbdc63
Sergey-ITProgrammer <sergey.rukin@icloud.com>
2022-07-10 19:15:30 +0500

    setup

---
+++ quick_resto_api/README.md
@@ -0,0 +1,5 @@
+# QuickRestoApi
+
+Реализация Quick Resto API на языке программирования Python
+
+https://quickresto.ru/api/description/intro/
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="quick_resto_api", 
+    version="1.0",
+    description='Quick Resto API',
+    packages=['quick_resto_api'],
+    author_email='sergey.rukin1425@gmail.com',
+    )
